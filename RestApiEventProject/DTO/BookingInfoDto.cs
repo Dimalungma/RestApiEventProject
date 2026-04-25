8c6a0c3597a4fbea32e8d2bdee247fdf3de767f4
Dimalungma <dimalungmawork@gmail.com>
2026-04-25 19:10:55 +0300

    вернул int в id, так как иначе не привязать к мероприятию

--- RestApiEventProject/DTO/BookingInfoDto.cs
+++ RestApiEventProject/DTO/BookingInfoDto.cs
@@ -6,7 +6,9 @@ public class BookingInfoDto
 {
     public Guid Id { get; set; }
 
-    public Guid EventId { get; set; }
+    public int EventId { get; set; } 
+    //Так как guid ломает все прошлые тесты, инициализацию, и потенциально БД, оставляю int,
+    //так как не вижу, какой прирост даст переход на guid, помимо геморроя с b-tree базой
 
     public BookingStatus Status { get; set; }
 
