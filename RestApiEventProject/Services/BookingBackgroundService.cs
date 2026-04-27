48ba11e025d8adfffa62d21d3e33c002bb785b66
Dimalungma <dimalungmawork@gmail.com>
2026-04-27 10:08:02 +0300

    Добавил сценарий Rejected

--- RestApiEventProject/Services/BookingBackgroundService.cs
+++ RestApiEventProject/Services/BookingBackgroundService.cs
@@ -31,6 +31,9 @@ public class BookingBackgroundService : BackgroundService
                 try
                 {
                     await Task.Delay(TimeSpan.FromSeconds(2), stoppingToken); //ОООООЧЕНЬ тяжелая операция
+                    Random payment = new Random();
+                    if (payment.Next(0, 5) == 5)
+                        throw new PaymentRejectedException("Бронь отменена, не прошла оплата (Random)");
 
                     var isConfirmed = await _bookingProcessingService.ConfirmBookingAsync(booking.Id);
 
@@ -41,13 +44,28 @@ public class BookingBackgroundService : BackgroundService
                 {
                     return;
                 }
+                catch (PaymentRejectedException exception)
+                {
+                    _logger.LogError(exception, $"Ошибка при оплате брони с id {booking.Id}, отменяю бронирование");
+                    var isRejected = await _bookingProcessingService.RejectBookingAsync(booking.Id);
+                    if (isRejected)
+                        _logger.LogInformation($"Бронь с id {booking.Id} отменена");
+                }
                 catch (Exception exception)
                 {
-                    _logger.LogError(exception, $"Ошибка при обработке брони с id {booking.Id}");
+                    _logger.LogError(exception, $"Неизвестная ошибка при обработке брони с id {booking.Id}"); 
+                    //Тут я не хочу отменять, так как неизвестно, по какому именно условию мы можем попасть в общий Exception, а значит сам Accept\Reject метод может дать exception
                 }
             }
 
             await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken); //Увеличил до 5 в рамках отладки, так как нереально отловить момент смены, сразу идет инициализация даже с Delay в 2 секунды.
         }
+    }
+}
+
+public class PaymentRejectedException : Exception
+{
+    public PaymentRejectedException(string message) : base(message)
+    {
     }
 }
